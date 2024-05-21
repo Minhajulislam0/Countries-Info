6c293dce4a5270560455e8fac916ce6ba00f9c0e
Minhajulislam0 <mdmishu98730@gmail.com>
2024-05-22 01:03:20 +0600

    Final

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -5,18 +5,32 @@ import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
       .then((data) => setCountries(data));
   }, []);
 
+  const handleVisitedCountries = (country) => {
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+
   return (
     <div>
       <h2>Countries: {countries.length}</h2>
+      <div>
+        <h3>Visited Countries: {visitedCountries.length}</h3>
+        <ul></ul>
+      </div>
       <div className="countries-container">
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country
+            handleVisitedCountries={handleVisitedCountries}
+            key={country.cca3}
+            country={country}
+          ></Country>
         ))}
       </div>
     </div>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,23 +1,29 @@
 import { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   const { name, flags, population, area, cca3 } = country;
   const [visited, setVisited] = useState(false);
   const handleVisit = () => {
     setVisited(!visited);
   };
+
+  const passVisitedCountry = () => {
+    handleVisitedCountries(country);
+  };
+
   return (
-    <div className="country">
+    <div className={`country ${visited && "visited"}`}>
       <h2>{name?.common}</h2>
       <img src={flags?.png} alt="flag" />
       <p>Total Population: {population}</p>
       <p>Total Land: {area}</p>
       <p>Code: {cca3}</p>
+      <button onClick={passVisitedCountry}>Mark Visited</button>
       <button onClick={handleVisit}>{visited ? "Visited" : "Going"}</button>
       {visited
-        ? "I have visited this country."
-        : "I want to visit this country."}
+        ? " I have visited this country."
+        : " I want to visit this country."}
     </div>
   );
 };
